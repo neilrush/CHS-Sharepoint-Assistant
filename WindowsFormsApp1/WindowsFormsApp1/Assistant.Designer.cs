c69064b43db749de6afe4eaf4bd1f2ea5df816c9
Neil Rush <neilrush@ksu.edu>
2017-12-20 08:59:44 -0600

    User Interface Setup

---
+++ WindowsFormsApp1/WindowsFormsApp1/Assistant.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class assistant
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.uxRun = new System.Windows.Forms.Button();
+            this.uxLoadFolder = new System.Windows.Forms.Button();
+            this.uxConsole = new System.Windows.Forms.TextBox();
+            this.uxOpenFolderDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.uxPath = new System.Windows.Forms.TextBox();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.uxFileLabel = new System.Windows.Forms.ToolStripDropDownButton();
+            this.LoadFolder = new System.Windows.Forms.ToolStripMenuItem();
+            this.uxRemoveLast = new System.Windows.Forms.ToolStripMenuItem();
+            this.uxClear = new System.Windows.Forms.ToolStripMenuItem();
+            this.Tools = new System.Windows.Forms.ToolStripDropDownButton();
+            this.uxAddDates = new System.Windows.Forms.ToolStripMenuItem();
+            this.uxReformatDates = new System.Windows.Forms.ToolStripMenuItem();
+            this.uxFolderList = new System.Windows.Forms.ListBox();
+            this.uxShowConsole = new System.Windows.Forms.Button();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // uxRun
+            // 
+            this.uxRun.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.uxRun.Location = new System.Drawing.Point(490, 111);
+            this.uxRun.Name = "uxRun";
+            this.uxRun.Size = new System.Drawing.Size(75, 23);
+            this.uxRun.TabIndex = 1;
+            this.uxRun.Text = "Run";
+            this.uxRun.UseVisualStyleBackColor = true;
+            this.uxRun.Click += new System.EventHandler(this.uxRun_Click);
+            // 
+            // uxLoadFolder
+            // 
+            this.uxLoadFolder.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.uxLoadFolder.Location = new System.Drawing.Point(286, 111);
+            this.uxLoadFolder.Name = "uxLoadFolder";
+            this.uxLoadFolder.Size = new System.Drawing.Size(75, 23);
+            this.uxLoadFolder.TabIndex = 2;
+            this.uxLoadFolder.Text = "Load Folder";
+            this.uxLoadFolder.UseVisualStyleBackColor = true;
+            this.uxLoadFolder.Click += new System.EventHandler(this.uxLoadFolder_Click);
+            // 
+            // uxConsole
+            // 
+            this.uxConsole.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.uxConsole.BackColor = System.Drawing.SystemColors.MenuText;
+            this.uxConsole.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.uxConsole.ForeColor = System.Drawing.SystemColors.Window;
+            this.uxConsole.Location = new System.Drawing.Point(12, 168);
+            this.uxConsole.Multiline = true;
+            this.uxConsole.Name = "uxConsole";
+            this.uxConsole.ReadOnly = true;
+            this.uxConsole.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.uxConsole.Size = new System.Drawing.Size(553, 0);
+            this.uxConsole.TabIndex = 0;
+            this.uxConsole.Text = "uxConsole";
+            this.uxConsole.Visible = false;
+            // 
+            // uxPath
+            // 
+            this.uxPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.uxPath.Location = new System.Drawing.Point(12, 111);
+            this.uxPath.Name = "uxPath";
+            this.uxPath.Size = new System.Drawing.Size(268, 20);
+            this.uxPath.TabIndex = 6;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.uxFileLabel,
+            this.Tools});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(577, 25);
+            this.toolStrip1.TabIndex = 7;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // uxFileLabel
+            // 
+            this.uxFileLabel.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.LoadFolder,
+            this.uxRemoveLast,
+            this.uxClear});
+            this.uxFileLabel.Name = "uxFileLabel";
+            this.uxFileLabel.Size = new System.Drawing.Size(38, 22);
+            this.uxFileLabel.Text = "File";
+            // 
+            // LoadFolder
+            // 
+            this.LoadFolder.Name = "LoadFolder";
+            this.LoadFolder.Size = new System.Drawing.Size(177, 22);
+            this.LoadFolder.Text = "Load Folder";
+            this.LoadFolder.Click += new System.EventHandler(this.LoadFolder_Click);
+            // 
+            // uxRemoveLast
+            // 
+            this.uxRemoveLast.Name = "uxRemoveLast";
+            this.uxRemoveLast.Size = new System.Drawing.Size(177, 22);
+            this.uxRemoveLast.Text = "Remove Last Folder";
+            this.uxRemoveLast.Click += new System.EventHandler(this.uxRemoveLast_Click);
+            // 
+            // uxClear
+            // 
+            this.uxClear.Name = "uxClear";
+            this.uxClear.Size = new System.Drawing.Size(177, 22);
+            this.uxClear.Text = "Clear Folders";
+            this.uxClear.Click += new System.EventHandler(this.uxClear_Click);
+            // 
+            // Tools
+            // 
+            this.Tools.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.uxAddDates,
+            this.uxReformatDates});
+            this.Tools.Name = "Tools";
+            this.Tools.Size = new System.Drawing.Size(49, 22);
+            this.Tools.Text = "Tools";
+            // 
+            // uxAddDates
+            // 
+            this.uxAddDates.CheckOnClick = true;
+            this.uxAddDates.Name = "uxAddDates";
+            this.uxAddDates.Size = new System.Drawing.Size(155, 22);
+            this.uxAddDates.Text = "Add Dates";
+            // 
+            // uxReformatDates
+            // 
+            this.uxReformatDates.CheckOnClick = true;
+            this.uxReformatDates.Name = "uxReformatDates";
+            this.uxReformatDates.Size = new System.Drawing.Size(155, 22);
+            this.uxReformatDates.Text = "Reformat Dates";
+            // 
+            // uxFolderList
+            // 
+            this.uxFolderList.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.uxFolderList.FormattingEnabled = true;
+            this.uxFolderList.Location = new System.Drawing.Point(13, 28);
+            this.uxFolderList.Name = "uxFolderList";
+            this.uxFolderList.Size = new System.Drawing.Size(553, 69);
+            this.uxFolderList.TabIndex = 8;
+            // 
+            // uxShowConsole
+            // 
+            this.uxShowConsole.Location = new System.Drawing.Point(13, 139);
+            this.uxShowConsole.Name = "uxShowConsole";
+            this.uxShowConsole.Size = new System.Drawing.Size(95, 23);
+            this.uxShowConsole.TabIndex = 9;
+            this.uxShowConsole.Text = "Show Console";
+            this.uxShowConsole.UseVisualStyleBackColor = true;
+            this.uxShowConsole.Click += new System.EventHandler(this.uxShowConsole_Click);
+            // 
+            // assistant
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
+            this.ClientSize = new System.Drawing.Size(577, 170);
+            this.Controls.Add(this.uxShowConsole);
+            this.Controls.Add(this.uxFolderList);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.uxPath);
+            this.Controls.Add(this.uxLoadFolder);
+            this.Controls.Add(this.uxRun);
+            this.Controls.Add(this.uxConsole);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MinimumSize = new System.Drawing.Size(593, 209);
+            this.Name = "assistant";
+            this.ShowIcon = false;
+            this.Text = "SharePoint Assistant";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button uxRun;
+        private System.Windows.Forms.Button uxLoadFolder;
+        private System.Windows.Forms.TextBox uxConsole;
+        private System.Windows.Forms.FolderBrowserDialog uxOpenFolderDialog;
+        private System.Windows.Forms.TextBox uxPath;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripDropDownButton uxFileLabel;
+        private System.Windows.Forms.ToolStripMenuItem LoadFolder;
+        private System.Windows.Forms.ToolStripMenuItem uxRemoveLast;
+        private System.Windows.Forms.ToolStripMenuItem uxClear;
+        private System.Windows.Forms.ListBox uxFolderList;
+        private System.Windows.Forms.ToolStripDropDownButton Tools;
+        private System.Windows.Forms.ToolStripMenuItem uxAddDates;
+        private System.Windows.Forms.ToolStripMenuItem uxReformatDates;
+        private System.Windows.Forms.Button uxShowConsole;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Assistant.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApp1
+{
+    public partial class assistant : Form
+    {
+        /// <summary>
+        /// stores the folders to be processed in order
+        /// </summary>
+        private Stack<String> _folders = new Stack<string>();
+        public assistant()
+        {
+            InitializeComponent();
+            uxShowConsole.Text = ShowConsoleText[0];
+            blankText = uxPath.Text;
+        }
+#region Show/Hide Console
+        /// <summary>
+        /// The strings to iterate through for the console button
+        /// </summary>
+        private String[] ShowConsoleText = { "Show Console", "Hide Console" };
+        /// <summary>
+        /// used to iterate through the strings
+        /// </summary>
+        private int ConsoleIndex = 0;
+        /// <summary>
+        /// Toggles the button text, resizeability and if the console is visible or not
+        /// Default State is hidden
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void uxShowConsole_Click(object sender, EventArgs e)
+        {
+            if (ConsoleIndex == 0)//show console
+            {
+                uxShowConsole.Text = ShowConsoleText[1];
+                ConsoleIndex = 1;
+                this.MinimumSize = new Size(593, 323);
+                uxConsole.Visible = true;
+                this.FormBorderStyle = FormBorderStyle.Sizable;
+            }
+            else//hide console
+            {
+                uxShowConsole.Text = ShowConsoleText[0];
+                ConsoleIndex = 0;
+                this.MinimumSize = new Size(593, 209);
+                this.Size = new Size(593, 209);
+                uxConsole.Visible = false;
+                this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            }
+        }
+        #endregion
+
+
+#region Folder Loading
+        private void uxLoadFolder_Click(object sender, EventArgs e)
+        {
+            uxFolderList.Items.Add(uxPath.Text);
+            _folders.Push(uxPath.Text);
+            uxPath.Clear();
+        }
+        string blankText;
+        private void LoadFolder_Click(object sender, EventArgs e)
+        {
+            if (uxOpenFolderDialog.ShowDialog() == DialogResult.OK)
+            {
+                if (uxPath.Text.Trim().Equals(blankText))
+                {
+                    MessageBox.Show("You must enter a path");
+                }
+                else
+                {
+                    uxFolderList.Items.Add(uxOpenFolderDialog.SelectedPath);
+                    _folders.Push(uxOpenFolderDialog.SelectedPath);
+                }
+
+            }
+        }
+
+        private void uxRemoveLast_Click(object sender, EventArgs e)
+        {
+            string remove =_folders.Pop();
+            for (int n = uxFolderList.Items.Count - 1; n >= 0; --n)
+            {
+                if (uxFolderList.Items[n].ToString().Contains(remove))
+                {
+                    uxFolderList.Items.RemoveAt(n);
+                }
+            }
+        }
+#endregion
+        private void uxClear_Click(object sender, EventArgs e)
+        {
+            _folders.Clear();
+            uxFolderList.Items.Clear();
+            uxPath.Clear();
+        }
+
+        private void uxRun_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
